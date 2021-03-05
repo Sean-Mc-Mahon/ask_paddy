642ac14da199f3e094fe558b0fca8f54a81751e9
Sean-Mc-Mahon <sean.mcmahon87@gmail.com>
2021-03-05 17:03:18 +0000

    quiz added

--- app.js
+++ app.js
@@ -1,3 +1,103 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('q')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+function startGame() {
+    console.log('Started');
+    startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion();
+}
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button =>{
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const questions = [
+    {
+        question: "When is St.Patrick's Day?",
+        answers: [
+            {text: "March 17th", correct: true},
+            {text: "Smarch 17th", correct: false},
+            {text: "March 10th", correct: false},
+            {text: "April 20th", correct: false}
+        ]
+    },
+    {
+        question: "What is the significance of March 17 to Patrick?",
+        answers: [
+            {text: "His first Guinness", correct: false},
+            {text: "His Birthday", correct: false},
+            {text: "The day he died", correct: true},
+            {text: "Chased out the snakes", correct: false}
+        ]
+    }
+]
+
 //DARK MODE
 var checkBox = document.getElementById("switch");
 var theme = window.localStorage.getItem("data-theme");
@@ -23,3 +123,5 @@ $('#paddy').on('mouseenter', () => {
 }).on('mouseleave', () => {
     $('#paddy').attr('src', 'static/images/paddy.png');
 });
+
+
